7eba920faaf6e2d9fca4f72dc06a4b35db095dd3
imnofox <pocketmines@mcpestuffs.com>
2016-02-23 23:33:52 +1300

    Init

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+const path = require('path');
+
+var router = function(path) {
+  if (m = path.match(/src\/texture\/(.*)/)) {
+    return m[1];
+  } else if (m = path.match(/build\/(.*)/)) {
+    return 'script/' + m[1];
+  }
+  return path;
+};
+
+module.exports = function(grunt) {
+  const pkg = grunt.file.readJSON('package.json');
+  const ftp = grunt.file.readJSON('.ftppass');
+
+  grunt.initConfig({
+    pkg: pkg,
+    'ftp-deploy': {
+      build: {
+        auth: {
+          host: ftp['key'].ip,
+          port: ftp['key'].port,
+          authKey: 'key'
+        },
+        src: 'temp',
+        dest: '/_modpe_scripts'
+      }
+    },
+    browserify: {
+      dist: {
+        options: {
+          transform: [
+            ['babelify']
+          ]
+        },
+        src: 'src/' + pkg.name + '.js',
+        dest: 'build/' + pkg.name + '.js'
+      },
+      test: {
+        options: {
+          transform: [
+            ['babelify']
+          ]
+        },
+        src: 'src/' + pkg.name + '.js',
+        dest: 'temp/' + pkg.name + '.js'
+      }
+    },
+    uglify: {
+      build: {
+        src: 'build/' + pkg.name + '.js',
+        dest: 'build/' + pkg.name + '.min.js'
+      },
+      test: {
+        src: 'temp/' + pkg.name + '.js',
+        dest: 'temp/' + pkg.name + '.min.js'
+      }
+    },
+    zip: {
+      'test': {
+        router: router,
+        src: ['temp/manifest.json', 'src/texture/*', 'temp/*.min.js'],
+        dest: 'temp/' + pkg.name + '.modpkg'
+      },
+      'build': {
+        router: router,
+        src: ['temp/manifest.json', 'src/texture/*', 'build/*.min.js'],
+        dest: 'build/' + pkg.name + '.modpkg'
+      }
+    },
+    "file-creator": {
+      basic: {
+        "temp/manifest.json": function(fs, fd, done) {
+          fs.writeSync(fd, JSON.stringify({
+            name: pkg.name,
+            author: pkg.author,
+            description: pkg.description,
+            version: pkg.version
+          }));
+          done();
+        }
+      }
+    },
+    clean: ['temp/']
+  });
+
+  grunt.loadNpmTasks('grunt-ftp-deploy');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-zip');
+  grunt.loadNpmTasks('grunt-file-creator');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('test', ['browserify:test', 'uglify:test', 'file-creator', 'zip:test', 'ftp-deploy', 'clean']);
+  grunt.registerTask('default', ['browserify', 'uglify', 'file-creator', 'zip:build']);
+
+};
---
+++ readme.md
@@ -0,0 +1,31 @@
+# ModPE + Node Template
+
+Clone this repo, then run `$ npm install`.
+Make sure Grunt is installed.
+
+## Building
+
+This utilizes Grunt.
+Simply run `$ grunt`
+
+- Uses browserify to build into one file with module support
+- Uses babel to support ES6 syntax in ModPE
+
+### Testing
+
+Run `$ grunt test`
+
+Make sure you have [an FTP server running on your testing device](https://play.google.com/store/apps/details?id=com.theolivetree.ftpserver), and a config file named `.ftppass`.
+
+`.ftppass` should have the format:
+```
+{
+  "key": {
+		"ip": "192.168.1.xx",
+		"port": 2221,
+    "username": "xxxx",
+    "password": "xxxx"
+  }
+}
+
+```
